7d593a17fa0dd3ac8a6add0e0a790718913555d8
nwagu chima <chima.nwagu@decagon.dev>
2022-03-27 16:01:01 +0100

    completed header component

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -93,3 +93,19 @@ export const NavMenu = styled.div`
     /* display: none; */
   }
 `;
+
+export const Login = styled.a`
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Nav, Logo, NavMenu } from "./Header.styles";
+import { Nav, Logo, NavMenu, Login } from "./Header.styles";
 
 const Header = () => {
   return (
@@ -10,9 +10,30 @@ const Header = () => {
       <NavMenu>
         <a href="/home">
           <img src="/images/home-icon.svg" alt="Home" />
-          <span>Home</span>
+          <span>HOME</span>
+        </a>
+        <a href="/home">
+          <img src="/images/search-icon.svg" alt="SEARCH" />
+          <span>SEARCH</span>
+        </a>
+        <a href="/home">
+          <img src="/images/watchlist-icon.svg" alt="WATCHLIST" />
+          <span>WATCHLIST</span>
+        </a>
+        <a href="/home">
+          <img src="/images/original-icon.svg" alt="ORIGINALS" />
+          <span>ORIGINALS</span>
+        </a>
+        <a href="/home">
+          <img src="/images/movie-icon.svg" alt="MOVIES" />
+          <span>MOVIES</span>
+        </a>
+        <a href="/home">
+          <img src="/images/series-icon.svg" alt="SERIES" />
+          <span>SERIES</span>
         </a>
       </NavMenu>
+      <Login>Login</Login>
     </Nav>
   );
 };

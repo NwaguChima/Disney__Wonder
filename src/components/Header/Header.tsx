3fac2d3ff5a1a86680f6e948cbc401d0847ef24f
nwagu chima <chima.nwagu@decagon.dev>
2022-03-27 15:48:13 +0100

    implemented slide in hover

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 import React from "react";
-import { Nav, Logo } from "./Header.styles";
+import { Nav, Logo, NavMenu } from "./Header.styles";
 
 const Header = () => {
   return (
     <Nav>
-      <Logo>
+      <Logo href="#">
         <img src="/images/logo.svg" alt="Disney logo" />
       </Logo>
+      <NavMenu>
+        <a href="/home">
+          <img src="/images/home-icon.svg" alt="Home" />
+          <span>Home</span>
+        </a>
+      </NavMenu>
     </Nav>
   );
 };

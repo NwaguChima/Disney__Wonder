cda0d7ecc794d55b26348d4da284d1f9b06b382a
nwagu chima <chima.nwagu@decagon.dev>
2022-04-02 10:27:00 +0100

    viewers video added and completed

--- src/components/Home/Viewers/Viewers.tsx
+++ src/components/Home/Viewers/Viewers.tsx
@@ -6,18 +6,36 @@ const Viewers = (props: any) => {
     <Container>
       <Wrap>
         <img src="/images/viewers-disney.png" alt="disney_images" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564674844-disney.mp4" typeof="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-pixar.png" alt="disney_images" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676714-pixar.mp4" typeof="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-marvel.png" alt="disney_images" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1564676115-marvel.mp4" typeof="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-starwars.png" alt="disney_images" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source src="/videos/1608229455-star-wars.mp4" typeof="video/mp4" />
+        </video>
       </Wrap>
       <Wrap>
         <img src="/images/viewers-national.png" alt="disney_images" />
+        <video autoPlay={true} loop={true} playsInline={true}>
+          <source
+            src="/videos/1564676296-national-geographic.mp4"
+            typeof="video/mp4"
+          />
+        </video>
       </Wrap>
     </Container>
   );
